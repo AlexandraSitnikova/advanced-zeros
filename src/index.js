11d0e1452a9de1df915c9e24b41624c0f165c384
AlexandraSitnikova <sitnikovalexandra@yandex.by>
2018-09-13 18:46:33 +0300

    zeros in number! in base

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let divider = [];
+  let minResult = null;
+
+  if (base > 0 && base != 1) {
+    for (let i = 2; i <= base; i++) {
+      while (base % i == 0 && base > 1) {
+        if (!divider.includes(i)) {
+          divider.push(i);
+        } else {
+          divider[divider.indexOf(i)] += i;
+        }
+        base /= i;
+      }
+      if (base / i == 1) {
+        if (!divider.includes(i)) {
+          divider.push(x);
+        } else {
+          divider[divider.indexOf(i)] += i;
+        }
+      }
+    }
+  } else if (base == 1) {
+    divider.push(1);
+  }
+
+  divider.forEach(element => {
+    let bufNum = number;
+    let result = 0;
+    while (bufNum > 0) {
+      bufNum = parseInt(bufNum / element);
+      result += bufNum;
+    }
+    if (result < minResult || !minResult) {
+      minResult = result;
+    }
+  })
+
+  return minResult; 
 }
+
